8752f321d81666f76b8a90b785e90171782396d4
mohammedasimahmed <asim60999@gmail.com>
2024-01-04 22:17:24 +0530

    overview section added

--- client/components/molecules/home/OverviewComponent.jsx
+++ client/components/molecules/home/OverviewComponent.jsx
@@ -1,19 +1,39 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 import { motion, useAnimation } from "framer-motion";
 import { useInView } from "react-intersection-observer";
 
 const OverviewComponent = () => {
+  const controls = useAnimation();
+  const [ref, inView] = useInView();
+
+  const animationVariants = {
+    visible: { opacity: 1, y: 0 },
+    hidden: { opacity: 0, y: 50 },
+  };
+
+  const animationOptions = {
+    variants: animationVariants,
+    initial: "hidden",
+    animate: inView && "visible",
+    transition: { duration: 0.5 },
+  };
+
+  useEffect(() => {
+    if (inView) {
+        controls.start("visible")
+    }
+  }, [controls, inView]);
   return (
-    <div className="w-screen h-screen flex flex-col px-20 justify-center items-start flex-wr">
-      {/* <motion.div>
-        <div className="text-7xl">
+    <div className="w-screen h-screen opacity-30 flex px-20 justify-center items-center flex-wr">
+      <motion.div ref={ref} animate={controls} {...animationOptions}>
+        <div className="text-7xl text-white text-center">
           Welcome to Hackofiesta 2024! Join us for an exhilarating hackathon.
           Collaborate with brilliant minds, innovate, and code for
           groundbreaking solutions. This annual event is your chance to shape
           the future through creativity. Become a part of Hackofiesta 2024!
         </div>
-      </motion.div> */}
+      </motion.div>
     </div>
   );
 };
