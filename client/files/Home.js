ce6116fe56a7822e6f1004d008b31826664109f1
Ankit Yadav <mahesh9426346889@gmail.com>
2024-01-05 14:06:02 +0530

    saving changes

--- client/files/Home.js
+++ client/files/Home.js
@@ -4,9 +4,9 @@ import Navbar from "@/components/molecules/common/Navbar";
 import HeroSection from "@/components/molecules/home/HeroSection";
 import React, { useEffect, useState } from "react";
 import CustomScrollbar from "@/components/atoms/CustomScrollbar";
-import OverviewComponent from "@/components/molecules/home/OverviewComponent";
 import TextSplitEffect from "@/components/molecules/home/TextSplitEffect";
 import Stats from "@/components/molecules/home/Stats";
+import { MobileNavbar } from "@/components/molecules/mobile-navbar/MobileNavbar";
 
 const Home = () => {
   const [val, setVal] = useState(false);
@@ -40,7 +40,7 @@ const Home = () => {
           {/* <OverviewComponent /> */}
           <TextSplitEffect />
           <Stats />
-        </div>
+        </>
       )}
     </>
   );
