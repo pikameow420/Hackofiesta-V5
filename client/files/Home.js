e998f76954bc3dc07b84d2c309783a6adbcb8ed0
mohammedasimahmed <asim60999@gmail.com>
2024-01-05 14:09:48 +0530

    removed overview component

--- client/files/Home.js
+++ client/files/Home.js
@@ -4,7 +4,6 @@ import Navbar from "@/components/molecules/common/Navbar";
 import HeroSection from "@/components/molecules/home/HeroSection";
 import React, { useEffect, useState } from "react";
 import CustomScrollbar from "@/components/atoms/CustomScrollbar";
-import OverviewComponent from "@/components/molecules/home/OverviewComponent";
 import TextSplitEffect from "@/components/molecules/home/TextSplitEffect";
 import Stats from "@/components/molecules/home/Stats";
 
@@ -36,7 +35,6 @@ const Home = () => {
           <Navbar />
           <HeroSection />
           <CustomScrollbar />
-          {/* <OverviewComponent /> */}
           <TextSplitEffect />
           <Stats />
         </div>
