528ba810d7d0c3527f1cde1239159ccad22445b2
Ankit Yadav <mahesh9426346889@gmail.com>
2024-01-13 04:09:37 +0530

    gradient background added in sponsors section

---
+++ client/components/atoms/gradientBackground.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const BgGradient = () => {
+  return (
+    <div className="-z-25 bg-zinc-950 flex justify-between absolute inset-0 overflow-hidden items-center outerdiv_style h-full w-full">
+      <div className="gradient"></div>
+      <div className="gradient"></div>
+    </div>
+  )
+}
+
+export default BgGradient
