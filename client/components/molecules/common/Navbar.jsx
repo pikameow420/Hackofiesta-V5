7c2ec668d67e8cebbef6c296bb0b67a11c8b1f31
Ankit Yadav <mahesh9426346889@gmail.com>
2024-01-13 03:29:19 +0530

    Navbar onscroll mobile view fixed

--- client/components/molecules/common/Navbar.jsx
+++ client/components/molecules/common/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
   }, []);
 
   return (
-    <div ref={refNav} className="flex sticky sm:justify-normal justify-between top-0 backdrop-blur-md z-10">
+    <div ref={refNav} className="flex sticky min-h-24 sm:justify-normal justify-between top-0 backdrop-blur-md z-10">
       <img src={"/IIITL.png"} width={76} className="absolute ms-5 mt-2" />
       <div className="md:flex hidden gap-20 self-center justify-center text-xl px-10 py-8 word-spacing tracking-tight text-white  w-full top-0 z-10">
         <Link
