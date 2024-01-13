5ab093e86354e40d5c124955bca4a1182e29fd8d
Prince Singh <marvelousprince012233@gmail.com>
2024-01-13 20:54:32 +0530

    seo og GA

--- client/app/layout.js
+++ client/app/layout.js
@@ -1,18 +1,83 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import MouseTrail from "@/components/molecules/common/MouseTrail";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "HACKOFIESTA",
-  description: "",
+  title: "Hackofiesta 5.0",
+  description:
+    "Tune in to 36 hours of creativity, coding and collaboration leading to some of the most innovative hacks you have ever seen. Hackofiesta is the biggest offline hackathon in Uttar Pradesh, conducted by IIIT Lucknow.",
+  keywords: [
+    "hackofiesta",
+    "hackathon",
+    "offline hackathon",
+    "IIIT Lucknow",
+    "hackathon in lucknow",
+    "hackathon in india",
+    "hackathon in uttar pradesh",
+    "hackathon in up",
+    "hackathon in iiit lucknow",
+    "hackathon in iiitl",
+  ],
+  canonical: "https://www.hackofiesta.com/",
+  metadataBase: new URL("https://www.hackofiesta.com"),
+  openGraph: {
+    url: "https://www.hackofiesta.com/",
+    title: "Hackofiesta 5.0",
+    description:
+      "Tune in to 36 hours of creativity, coding and collaboration leading to some of the most innovative hacks you have ever seen. Hackofiesta is the biggest offline hackathon in Uttar Pradesh, conducted by IIIT Lucknow.",
+    images: [
+      {
+        url: "/og1.png",
+        width: 1920,
+        height: 911,
+        alt: "Hackofiesta Landing Page",
+        type: "image/jpeg",
+      },
+      {
+        url: "/og2.png",
+        width: 500,
+        height: 500,
+        alt: "Hackofiesta Logo",
+        type: "image/png",
+      },
+    ],
+    siteName: "Hackofiesta 5.0",
+    robots: {
+      index: true,
+      follow: true,
+      nocache: true,
+      googleBot: {
+        index: true,
+        follow: true,
+        "max-video-preview": -1,
+        "max-image-preview": "large",
+        "max-snippet": -1,
+      },
+    },
+  },
+  twitter: {
+    handle: "@hof_iiitl",
+    cardType: "summary_large_image",
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Script src="https://www.googletagmanager.com/gtag/js?id=KCPPJMEM4B" />
+        <Script id="google-analytics">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', 'KCPPJMEM4B');
+        `}
+        </Script>
         <MouseTrail />
         {children}
       </body>
