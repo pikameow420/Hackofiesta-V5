52cdde2db15b06b2e1f0852a329353f845f60680
monarxxjain <jainmonark1000@gmail.com>
2024-01-13 13:11:43 +0530

    Changes By asim

--- client/components/molecules/home/About.jsx
+++ client/components/molecules/home/About.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef, useState } from "react";
 import TextReveal from "./TextReveal";
 import { motion, useAnimation } from "framer-motion";
+import { Hero, HeroProps } from "@lobehub/ui";
 import { useInView } from "react-intersection-observer";
 import { useRouter } from "next/navigation";
 import { myFont } from "../common/AnimatedTitle";
@@ -11,6 +12,8 @@ import {
   useControls,
   useCreateStore,
 } from '@lobehub/ui';
+
+import { ThemeProvider } from "@lobehub/ui";
 
 const About = () => {
   const store = useCreateStore();
@@ -71,47 +74,61 @@ const About = () => {
   //   }
   // }, [scrollDiv]);
   return (
-    <motion.div
-      ref={ref}
-      animate={controls}
-      {...animationOptions}
-      className="flex flex-col"
-    >
-      <div
-        className="w-screen sm:pl-12 pr-8 ms-16 flex flex-col-reverse sm:flex-row items-center justify-around"
-        style={{ height: "50vh" }}
+    <ThemeProvider themeMode="dark">
+      <motion.div
+        ref={ref}
+        animate={controls}
+        {...animationOptions}
+        className="flex flex-col"
       >
-        <motion.div>
-          <h1 className={`text-3xl mb-2 lg:mb-3 xl:-mb-1 text-center ${myFont.className} sm:text-5xl xl:text-6xl xl:mt-8 font-[mokoto]`}  >
-            HackoFiesta
-          </h1>
-          <div className="mr-4 sm:mr-0 text-gray-500 text-center sm:px-2 w-screen text-base xl:text-2xl lg:text-2xl xl:w-[500px] xl:p-6 md:text-2xl md:w-[350px] sm:text-xl sm:w-[250px] sm:ml-8 sm:p-0">
-            It is the biggest offline hackathon in Uttar Pradesh, conducted by IIIT
-            Lucknow. Tune in to 36 hours of creativity, coding and collaboration
-            leading to some of the most innovative hacks you have ever seen.
-          </div>
-          <div className="flex justify-center">
-            <button className="button-glitch mx-5 flex items-center my-2">
+        <div
+          className="w-screen sm:pl-12 pr-8 ms-16 flex flex-col-reverse sm:flex-row items-center justify-around"
+          style={{ height: "50vh" }}
+        >
+          <motion.div>
+            <h1 className={`text-3xl mb-2 lg:mb-3 xl:-mb-1 text-center ${myFont.className} sm:text-5xl xl:text-6xl xl:mt-8 font-[mokoto]`}  >
+              HackoFiesta
+            </h1>
+            <div className="mr-4 sm:mr-0 text-gray-500 text-center sm:px-2 w-screen text-base xl:text-2xl lg:text-2xl xl:w-[500px] xl:p-6 md:text-2xl md:w-[350px] sm:text-xl sm:w-[250px] sm:ml-8 sm:p-0">
+              It is the biggest offline hackathon in Uttar Pradesh, conducted by IIIT
+              Lucknow. Tune in to 36 hours of creativity, coding and collaboration
+              leading to some of the most innovative hacks you have ever seen.
+            </div>
+            <div className="flex justify-center btnDiv">
+              {/* <button className="button-glitch mx-5 flex items-center my-2">
               Know More
-            </button>
-          </div>
-        </motion.div>
-        <div className="flex justify-center xl:mr-32 xl:mt-28 lg:mt-24 md:mt-20 sm:mt-16">
-          <motion.div className="flex sm:justify-center">
-            <div
-              className="flex w-[250px] lg:w-[375px] md:w-[300px] sm:w-[250px] xl:w-[420px] "
-              id="creatorsPoint"
-            >
-              <img
-                src="/about_gif.gif"
-                className="w-full h-full"
-                alt=""
+            </button> */}
+
+              <Hero
+                actions={[{
+                  icon: "Discord",
+                  link: "https://github.com/lobehub",
+                  text: "Discord",
+                  type: "primary",
+                }]}
+              // description="Lobe UI is an open-source UI component library for <br/>building chatbot web apps"
+              // title="Lobe <b>UI</b>"
               />
+
             </div>
           </motion.div>
+          <div className="flex justify-center xl:mr-32 xl:mt-28 lg:mt-24 md:mt-20 sm:mt-16">
+            <motion.div className="flex sm:justify-center">
+              <div
+                className="flex w-[250px] lg:w-[375px] md:w-[300px] sm:w-[250px] xl:w-[420px] "
+                id="creatorsPoint"
+              >
+                <img
+                  src="/about_gif.gif"
+                  className="w-full h-full"
+                  alt=""
+                />
+              </div>
+            </motion.div>
+          </div>
         </div>
-      </div>
-    </motion.div>
+      </motion.div>
+    </ThemeProvider>
   );
 };
 
